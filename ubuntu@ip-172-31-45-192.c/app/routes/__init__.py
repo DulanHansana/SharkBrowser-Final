b86c3775c0a1a54211e3139ba8a2e709c0754fce
DulanHansana <146365204+DulanHansana@users.noreply.github.com>
2025-10-28 18:28:26 -0400

    Delete ubuntu@ip-172-31-45-192.c directory

--- ubuntu@ip-172-31-45-192.c/app/routes/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-API Routes Package
-
-This package contains all the API route modules for the SharkBrowser API.
-"""
-
-from . import sessions, health
-
-__all__ = ["sessions", "health"]
--- ubuntu@ip-172-31-45-192.c/app/services/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Services Package
-
-This package contains the core business logic services for the SharkBrowser API.
-"""
-
-from . import browser_manager
-
-__all__ = ["browser_manager"]
